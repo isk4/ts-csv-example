080865e03fd943ae67462e8c9abb8726952f9581
isk4 <vgutierrez@protonmail.com>
2025-06-02 12:46:26 -0400

    Use HTML report generator with summaries

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { CsvFileReader } from './CsvFileReader';
 import { parseMatchRow } from './utils';
 import { Summary } from './Summary';
-import { WinsAnalyzer } from './analyzers/WinsAnalyzer';
+// import { WinsAnalyzer } from './analyzers/WinsAnalyzer';
 import { AverageGoalsAnalyzer } from './analyzers/AverageGoalsAnalyzer';
-import { ConsoleReportGenerator } from './reportGenerators/ConsoleReportGenerator';
+// import { ConsoleReportGenerator } from './reportGenerators/ConsoleReportGenerator';
+import { HtmlReportGenerator } from './reportGenerators/HtmlReportGenerator';
 
 const reader = new CsvFileReader('football.csv', parseMatchRow);
 reader.read();
@@ -11,7 +12,7 @@ reader.read();
 // Example use
 const summary = new Summary(
     new AverageGoalsAnalyzer('Man United'),
-    new ConsoleReportGenerator()
+    new HtmlReportGenerator()
 );
 
 summary.generate(reader.data);
